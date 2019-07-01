dfb5392fece5aa8ac2d3e00bf2dc29df95fc192d
rayyyy <nraavy@gmail.com>
2019-07-02 08:34:29 +0900

    本番用の環境 #10

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  // TODO: 本番用の環境にする
+  firebase: {
+    apiKey: "AIzaSyDyeLcBY1JcrbyFGb_wqwKFrQvWaQF2XeA",
+    authDomain: "muscle-2710.firebaseapp.com",
+    databaseURL: "https://muscle-2710.firebaseio.com",
+    projectId: "muscle-2710",
+    storageBucket: "",
+    messagingSenderId: "775392555384",
+    appId: "1:775392555384:web:d4dcdef7cc1bbe7c"
+  }
 };
