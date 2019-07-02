ca59360a4ef98537202095fed406cc2ada969e13
rayyyy <nraavy@gmail.com>
2019-07-02 13:10:23 +0900

    遷移するようにした

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
+  // redirectUrl: string; // ログインしたときのページに戻ったりする時に使いたい
 
   constructor(
-    private angularFireAuth: AngularFireAuth
+    private angularFireAuth: AngularFireAuth,
+    private router: Router
   ) { }
 
   isLoggedIn() {
@@ -17,9 +20,11 @@ export class AuthService {
   async signUp(email: string, pass: string) {
     try {
       const credential = await this.angularFireAuth.auth.createUserWithEmailAndPassword(email, pass);
+      this.router.navigate(['/']);
     } catch (error) {
       if (error.code = 'auth/email-already-in-use') {
         // すでに使われている場合は、ログインに切り替える
+        this.router.navigate(['/']);
         console.log('mailaddressが登録済み');
       }
       console.log(error);
