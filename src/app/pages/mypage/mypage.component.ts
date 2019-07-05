885f5cf170fe6132161ebf18e75805a754001a68
rayyyy <nraavy@gmail.com>
2019-07-05 23:36:10 +0900

    mypage追加

---
+++ src/app/pages/mypage/mypage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mypage',
+  templateUrl: './mypage.component.html',
+  styleUrls: ['./mypage.component.sass']
+})
+export class MypageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
