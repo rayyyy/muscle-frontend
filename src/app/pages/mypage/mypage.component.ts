180ae53652677c05f03252232b60a56762f24ff3
rayyyy <nraavy@gmail.com>
2019-07-06 00:18:08 +0900

    logout bt

--- src/app/pages/mypage/mypage.component.ts
+++ src/app/pages/mypage/mypage.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-mypage',
   templateUrl: './mypage.component.html',
-  styleUrls: ['./mypage.component.sass']
+  styleUrls: ['./mypage.component.sass'],
+  providers: [AuthService]
 })
 export class MypageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
   }
 
+  signOut() {
+    this.authService.signOut();
+  }
 }
--- src/app/pages/mypage/mypage.module.ts
+++ src/app/pages/mypage/mypage.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MypageRoutingModule } from './mypage-routing.module';
 import { MypageComponent } from './mypage.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [MypageComponent],
   imports: [
     CommonModule,
-    MypageRoutingModule
+    MypageRoutingModule,
+    MatButtonModule
   ]
 })
 export class MypageModule { }
