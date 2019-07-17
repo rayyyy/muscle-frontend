9bbf90b688ab52ea1ed4cdfd9d901936de00e599
rayyyy <nraavy@gmail.com>
2019-07-17 23:53:23 +0900

    photo

--- src/app/pages/user/show/plan/show/show.module.ts
+++ src/app/pages/user/show/plan/show/show.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { ShowRoutingModule } from './show-routing.module';
 import { ShowComponent } from './show.component';
 import { BreadcrumbModule } from 'src/app/components/molecules/breadcrumb/breadcrumb.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [ShowComponent],
   imports: [
     CommonModule,
     ShowRoutingModule,
-    BreadcrumbModule
+    BreadcrumbModule,
+    FlexLayoutModule,
+    MatDividerModule
   ]
 })
 export class ShowModule { }
