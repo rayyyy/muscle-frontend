20fd58d473ee7891d46631b76b1d3b1cda298d96
rayyyy <nraavy@gmail.com>
2019-07-19 00:41:44 +0900

    add type

---
+++ src/app/interfaces/user-detail.ts
@@ -0,0 +1,9 @@
+export interface UserDetail {
+  id: number
+  user_id: number
+  short_message?: string
+  height?: number
+  weight?: number
+  created_at: string
+  updated_at: string
+}
---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: number
+  uid: string
+  image: string
+  email: string
+  nickname: string
+  sex?: string
+  status?: string
+  birthday?: string
+  created_at: string
+  updated_at: string
+}
