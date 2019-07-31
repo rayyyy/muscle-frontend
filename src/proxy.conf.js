642b4b8bcc2e2136b03a59f84bbda3c6004487b8
rayyyy <nraavy@gmail.com>
2019-07-31 11:01:29 +0900

    fix

--- src/proxy.conf.js
+++ src/proxy.conf.js
@@ -1,9 +1,12 @@
-const PROXY_CONFIG = [
-  {
-    context: ["/api"],
-    target: process.env.MUSCLE_API_URL || "http://app:5000",
-    secure: false
+const PROXY_CONFIG = {
+  "/api": {
+    "target": process.env.MUSCLE_API_URL || "http://app:5000",
+    "secure": false,
+    "pathRewrite": {
+      "^/api": ""
+    },
+    "changeOrigin": true
   }
-]
+}
 
 module.exports = PROXY_CONFIG;
