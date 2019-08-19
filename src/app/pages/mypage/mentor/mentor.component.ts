b2315b325990666d9b7ac25ade20bf484adaa4fb
rayyyy <nraavy@gmail.com>
2019-08-19 19:08:27 +0900

    パンくず

--- src/app/pages/mypage/mentor/mentor.component.ts
+++ src/app/pages/mypage/mentor/mentor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Breadcrumb } from 'src/app/interfaces/breadcrumb';
 
 @Component({
   selector: 'app-mentor',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mentor.component.sass']
 })
 export class MentorComponent implements OnInit {
+  sites: Breadcrumb[] = []
 
   constructor() { }
 
   ngOnInit() {
+    this.sites.push(
+      { pageName: 'ホーム', pageURL: '/' },
+      { pageName: 'マイページ', pageURL: '/mypage' },
+      { pageName: 'メンタープラン', pageURL: '/mypage/mentor' }
+    )
   }
 
 }
--- src/app/pages/mypage/mentor/mentor.module.ts
+++ src/app/pages/mypage/mentor/mentor.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MentorRoutingModule } from './mentor-routing.module';
 import { MentorComponent } from './mentor.component';
+import { BreadcrumbModule } from 'src/app/components/molecules/breadcrumb/breadcrumb.module';
 
 @NgModule({
   declarations: [MentorComponent],
   imports: [
     CommonModule,
-    MentorRoutingModule
+    MentorRoutingModule,
+    BreadcrumbModule
   ]
 })
 export class MentorModule { }
