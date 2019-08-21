c7fd92c7c8e8652d287f81d1e3146f99d76dfe9b
rayyyy <nraavy@gmail.com>
2019-08-21 21:52:09 +0900

    plan

--- src/app/pages/user/show/plan/plan.component.ts
+++ src/app/pages/user/show/plan/plan.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Breadcrumb } from 'src/app/interfaces/breadcrumb';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-plan',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plan.component.sass']
 })
 export class PlanComponent implements OnInit {
+  sites: Breadcrumb[] = []
+  userId: string
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.userId = this.route.snapshot.paramMap.get('user_id');
+    this.sites.push(
+      { pageName: "ホーム", pageURL: "/" },
+      { pageName: "tony", pageURL: `/user/${this.userId}` },
+      { pageName: "8月までにシックスパック", pageURL: `/user/${this.userId}/plan` }
+    )
   }
 
 }
--- src/app/pages/user/show/plan/plan.module.ts
+++ src/app/pages/user/show/plan/plan.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { PlanRoutingModule } from './plan-routing.module';
 import { PlanComponent } from './plan.component';
+import { BreadcrumbModule } from 'src/app/components/molecules/breadcrumb/breadcrumb.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [PlanComponent],
   imports: [
     CommonModule,
-    PlanRoutingModule
+    PlanRoutingModule,
+    BreadcrumbModule,
+    FlexLayoutModule,
+    MatDividerModule,
+    MatButtonModule
   ]
 })
 export class PlanModule { }
