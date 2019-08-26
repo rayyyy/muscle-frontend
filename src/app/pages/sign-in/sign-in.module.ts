9c74272e659b08c3c6b6b4b36e66b37f709dc4af
tom-nakamura <dnobwater246@gmail.com>
2019-08-26 22:45:06 +0900

    update:loginデザイン

--- src/app/pages/sign-in/sign-in.module.ts
+++ src/app/pages/sign-in/sign-in.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { SignInRoutingModule } from './sign-in-routing.module';
 import { SignInComponent } from './sign-in.component';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FormItemTitleModule } from 'src/app/components/atoms/form-item-title/form-item-title.module';
 
 @NgModule({
   declarations: [SignInComponent],
@@ -11,6 +14,10 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     SignInRoutingModule,
     FormsModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    FormItemTitleModule,
+    FormsModule
   ]
 })
-export class SignInModule { }
+export class SignInModule {}
